9b8d61ef2689159ebb31835dec62bb3683b0d58b
Leandro Struni <leandrostruni@gmail.com>
2025-03-06 17:06:36 +0100

    style: apply style component CategoryDropDownMenu

--- app/src/main/java/com/example/cocktailapi/components/CategoryDropdownMenu.kt
+++ app/src/main/java/com/example/cocktailapi/components/CategoryDropdownMenu.kt
@@ -1,5 +1,6 @@
 package com.example.cocktailapi.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +22,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.cocktailapi.ui.theme.NavyBlue
+import com.example.cocktailapi.ui.theme.SoftGold
+import com.example.cocktailapi.ui.theme.WineRed
 
 
 @Composable
@@ -34,9 +40,10 @@ fun CategoryDropdownMenu(
     Column {
         Button(
             onClick = { expanded = !expanded },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(containerColor = WineRed)
         ) {
-            Text("Seleccionar Categorías")
+            Text("Seleccionar Categorías", color = Color.White)
         }
 
         if (expanded) {
@@ -44,6 +51,7 @@ fun CategoryDropdownMenu(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(300.dp)
+                    .background(NavyBlue)
                     .padding(8.dp)
             ) {
                 items(categories) { category ->
@@ -54,7 +62,11 @@ fun CategoryDropdownMenu(
                             .clickable {
                                 selectedCategories.value =
                                     selectedCategories.value.toMutableSet().apply {
-                                        if (contains(category)) remove(category) else category?.let { add(it) }
+                                        if (contains(category)) remove(category) else category?.let {
+                                            add(
+                                                it
+                                            )
+                                        }
                                     }
                             }
                             .padding(8.dp)
@@ -70,8 +82,9 @@ fun CategoryDropdownMenu(
                         )
 
                         Spacer(modifier = Modifier.width(8.dp))
+
                         if (category != null) {
-                            Text(text = category)
+                            Text(text = category, color = SoftGold)
                         }
                     }
                 }
@@ -83,9 +96,10 @@ fun CategoryDropdownMenu(
                 expanded = false
                 onApplyFilters()
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(containerColor = WineRed)
         ) {
-            Text("Aplicar Filtros")
+            Text("Aplicar Filtros", color = Color.White)
         }
     }
 }
